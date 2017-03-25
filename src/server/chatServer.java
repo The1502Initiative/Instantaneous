58e0718c993adcfd5304b2249a9b3c7d101f0bcc
Simon\GitHub_Desktop <simon.seo@nyu.edu>
2017-03-25 15:19:45 +0800

    remove timer

--- src/server/chatServer.java
+++ src/server/chatServer.java
@@ -1,28 +1,14 @@
-import java.io.IOException;
 import java.net.*;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class chatServer {
 
 	public static void main(String[] args) throws Exception {
 		Database database = new Database();
 		int port = 3000;
-		int serverUpMinute = 3; 
 		InetAddress address = InetAddress.getLocalHost();
 		ServerSocket serverSocket = new ServerSocket(port);
+		System.out.println(serverSocket.getLocalSocketAddress());
 		System.out.println("Chat server listening on " + address + ":" + port);
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask(){
-			  @Override
-			  public void run() {
-			     try {
-					serverSocket.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			  }
-			}, serverUpMinute*60*1000);
         Socket connectionSocket = null;
 
 		while(true) { //run loop to wait for new connections
